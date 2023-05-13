48512ee351ff3c34ca42a799e96c7da7da86a0f7
Crowdbotics <team@crowdbotics.com>
2023-05-13 20:14:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "spring-moon-40672",
+  url: "https://spring-moon-40672.botics.co",
+  api: "https://spring-moon-40672.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
